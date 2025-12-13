77c3aa87048aadb5ab56db2f44857747f079d2ec
theaihopgg <theaihopgg@gmail.com>
2025-12-13 20:58:33 +0700

    fix: deleted postgres initial script

--- postgres_initial_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-# выглядит как костыль, потом заменить
-psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
-    CREATE DATABASE authorization_database;
-    CREATE USER authorization_api WITH PASSWORD '$AUTHORIZATION_API_USER_PASSWORD';
-    ALTER USER authorization_api WITH SUPERUSER;
-EOSQL
